214f3df659f2280511ff99712ff170dfe1f88c35
Betsue Weldemariam <btsegay0@gmail.com>
2023-12-20 20:03:51 +0300

    Added keyboard navigation using arrows

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ import contactImageMd from "@/public/contactPage-md.png"
 import downArrow from "@/public/downArrow.svg"
 
 import { motion, spring } from "framer-motion"
-import { WheelEvent, useEffect, useState } from "react"
+import { KeyboardEvent, WheelEvent, useEffect, useState } from "react"
 import Image from "next/image"
 
 export default function Home() {
@@ -28,31 +28,46 @@ export default function Home() {
   const [screenHeight, setScreenHeight] = useState<number>(768)
   const [currPage, setCurrPage] = useState<number>(1)
 
-  let page_counter = 1
+  //  Checks screen size and sets the appropriate image
 
   useEffect(() => {
+    //  Calculate the browser height to use proper image
+
     if (typeof window !== undefined) {
       setScreenHeight(window.innerHeight)
-      window.addEventListener("", () => {
-        setCurrPage(currPage + 1)
-        nextPage(currPage)
-      })
+      document.getElementById("focus")?.focus()
     }
   }, [])
+
+  //  Navigation using the scroll wheel
 
   function scroll(e: WheelEvent) {
     if (e.deltaY > 50) {
       setTimeout(() => {
         nextPage(currPage + 1)
       }, 300)
-      console.log(e.deltaY)
     } else if (e.deltaY < -50) {
       setTimeout(() => {
         previousPage(currPage - 1)
       }, 300)
-      console.log(e.deltaY)
     }
   }
+
+  //  Navigation using the arrow keys
+
+  function navigate(e: KeyboardEvent) {
+    if (e.key === "ArrowDown") {
+      setTimeout(() => {
+        nextPage(currPage + 1)
+      }, 300)
+    } else if (e.key === "ArrowUp") {
+      setTimeout(() => {
+        previousPage(currPage - 1)
+      }, 300)
+    }
+  }
+
+  //  Goes to the next page of the website
 
   function nextPage(n: number) {
     // y <= -300 ? setY(0) : setY(y - 100)
@@ -77,6 +92,8 @@ export default function Home() {
     }
   }
 
+  //  Goes to the previous page of the website
+
   function previousPage(n: number) {
     if (n !== currPage && n > 0) {
       document.getElementById(`button-${n}`)?.classList.remove("bg-white")
@@ -100,104 +117,110 @@ export default function Home() {
   }
 
   return (
-    <div
-      onWheel={(e) => scroll(e)}
-      className="w-screen h-screen flex flex-col overflow-hidden"
+    <button
+      id="focus"
+      className="absolute hover:cursor-default top-0 left-0 z-100 w-screen h-screen bg-white"
+      onKeyDown={navigate}
+      onWheel={scroll}
     >
-      <div className="absolute z-10 h-screen pl-5 flex flex-col justify-center gap-5">
-        <button
-          id="button-1"
-          onClick={() => nextPage(1)}
-          className="selector w-4 h-4 rounded-xl bg-wood"
-        ></button>
-        <button
-          id="button-2"
-          onClick={() => nextPage(2)}
-          className="selector w-4 h-4 rounded-xl bg-white"
-        ></button>
-        <button
-          id="button-3"
-          onClick={() => nextPage(3)}
-          className="selector w-4 h-4 rounded-xl bg-white"
-        ></button>
-        <button
-          id="button-4"
-          onClick={() => nextPage(4)}
-          className="selector w-4 h-4 rounded-xl bg-white"
-        ></button>
-      </div>
-      <motion.div
-        className="z-0"
-        animate={{ y: `${y}vh` }}
-        transition={{ type: "spring", stiffness: 30 }}
-      >
-        <div id="content-1" className="flex">
-          <ImageContainer
-            img_src={screenHeight < 768 ? homepageImageMd : homepageImageLg}
-            img_alt={"Woman slightly smiling and looking at camera"}
-          />
-          <TextContainer
-            heading="Fraol Zeleke"
-            desc="I am Dr. Fraol Zeleke, a neurology trailblazer. My groundbreaking diagnostic tool,
+      <div className="w-screen h-screen flex flex-col overflow-hidden">
+        <div className="absolute z-10 h-screen pl-5 flex flex-col justify-center gap-5">
+          <button
+            id="button-1"
+            onClick={() => nextPage(1)}
+            className="selector w-4 h-4 rounded-xl bg-wood shadow-xl shadow-black"
+          ></button>
+          <button
+            id="button-2"
+            onClick={() => nextPage(2)}
+            className="selector w-4 h-4 rounded-xl bg-white shadow-xl shadow-black"
+          ></button>
+          <button
+            id="button-3"
+            onClick={() => nextPage(3)}
+            className="selector w-4 h-4 rounded-xl bg-white shadow-xl shadow-black"
+          ></button>
+          <button
+            id="button-4"
+            onClick={() => nextPage(4)}
+            className="selector w-4 h-4 rounded-xl bg-white shadow-xl shadow-black"
+          ></button>
+        </div>
+        <motion.div
+          className="z-0"
+          animate={{ y: `${y}vh` }}
+          transition={{ type: "spring", stiffness: 30 }}
+        >
+          <div id="content-1" className="flex">
+            <ImageContainer
+              img_src={screenHeight < 768 ? homepageImageMd : homepageImageLg}
+              img_alt={"Woman slightly smiling and looking at camera"}
+            />
+            <TextContainer
+              heading="Fraol Zeleke"
+              desc="I am Dr. Fraol Zeleke, a neurology trailblazer. My groundbreaking diagnostic tool,
         patient-centric care, and commitment to mentorship define my legacy,
         recognized through accolades and a lasting impact on healthcare."
-            signature="Fraol Z."
-          />
+              signature="Fraol Z."
+            />
+          </div>
+          <div id="content-2" className="flex">
+            <ImageContainer
+              img_src={screenHeight < 768 ? educationImageMd : educationImageLg}
+              img_alt={"Woman writing on a piece of paper"}
+            />
+            <TextContainer
+              heading="Education"
+              desc="Dr. Fraol Zeleke excelled at the esteemed Aetherius Medical Academy, earning a Bachelor's in Neuroscience and later achieving a Master's and Ph.D. in Neurology. This imaginary institution shaped his commitment to patient-centric care and mentorship, laying the groundwork for his groundbreaking contributions to the field."
+              signature="Fraol Z."
+            />
+          </div>
+          <div id="content-3" className="flex">
+            <ImageContainer
+              img_src={
+                screenHeight < 768 ? experienceImageMd : experienceImageLg
+              }
+              img_alt={"Woman writing on a piece of paper"}
+            />
+            <TextContainer
+              heading="Experience"
+              desc="With a distinguished career, Dr. Fraol Zeleke has pioneered advancements in neurology. His research led to a groundbreaking diagnostic tool for neurological disorders. Beyond his achievements, his empathetic patient care and mentorship have inspired future healthcare professionals. Recognized with awards, Dr. Zeleke continues to shape the landscape of medical science."
+              signature="Fraol Z."
+            />
+          </div>
+          <div id="content-4" className="flex">
+            <ImageContainer
+              img_src={screenHeight < 768 ? contactImageMd : contactImageLg}
+              img_alt={"Woman writing on a piece of paper"}
+            />
+            <ContactContainer
+              heading="Contact"
+              desc="Let’s get in touch. You can find me through on one of the listed contact information."
+              phone={"251912345678"}
+              email="contact@fraol.me"
+              signature="Fraol Z."
+            />
+          </div>
+        </motion.div>
+        <div className="absolute bottom-0 flex justify-center w-screen">
+          <motion.button
+            onClick={() => nextPage(currPage + 1)}
+            initial={{ y: -8 }}
+            animate={{ y: -25 }}
+            exit={{ y: -8 }}
+            whileHover={{}}
+            transition={{
+              type: spring,
+              repeatType: "reverse",
+              repeat: Infinity,
+              duration: 1,
+            }}
+            className="w-12 h-12 rounded-[50%] bg-gray-100"
+          >
+            <Image src={downArrow} alt="down arrow" />
+          </motion.button>
         </div>
-        <div id="content-2" className="flex">
-          <ImageContainer
-            img_src={screenHeight < 768 ? educationImageMd : educationImageLg}
-            img_alt={"Woman writing on a piece of paper"}
-          />
-          <TextContainer
-            heading="Education"
-            desc="Dr. Fraol Zeleke excelled at the esteemed Aetherius Medical Academy, earning a Bachelor's in Neuroscience and later achieving a Master's and Ph.D. in Neurology. This imaginary institution shaped his commitment to patient-centric care and mentorship, laying the groundwork for his groundbreaking contributions to the field."
-            signature="Fraol Z."
-          />
-        </div>
-        <div id="content-3" className="flex">
-          <ImageContainer
-            img_src={screenHeight < 768 ? experienceImageMd : experienceImageLg}
-            img_alt={"Woman writing on a piece of paper"}
-          />
-          <TextContainer
-            heading="Experience"
-            desc="With a distinguished career, Dr. Fraol Zeleke has pioneered advancements in neurology. His research led to a groundbreaking diagnostic tool for neurological disorders. Beyond his achievements, his empathetic patient care and mentorship have inspired future healthcare professionals. Recognized with awards, Dr. Zeleke continues to shape the landscape of medical science."
-            signature="Fraol Z."
-          />
-        </div>
-        <div id="content-4" className="flex">
-          <ImageContainer
-            img_src={screenHeight < 768 ? contactImageMd : contactImageLg}
-            img_alt={"Woman writing on a piece of paper"}
-          />
-          <ContactContainer
-            heading="Contact"
-            desc="Let’s get in touch. You can find me through on one of the listed contact information."
-            phone={"251912345678"}
-            email="contact@fraol.me"
-            signature="Fraol Z."
-          />
-        </div>
-      </motion.div>
-      <div className="absolute bottom-0 flex justify-center w-screen">
-        <motion.button
-          onClick={() => nextPage(currPage + 1)}
-          initial={{ y: -8 }}
-          animate={{ y: -25 }}
-          exit={{ y: -8 }}
-          whileHover={{}}
-          transition={{
-            type: spring,
-            repeatType: "reverse",
-            repeat: Infinity,
-            duration: 1,
-          }}
-          className="w-12 h-12 rounded-[50%] bg-gray-100"
-        >
-          <Image src={downArrow} alt="down arrow" />
-        </motion.button>
       </div>
-    </div>
+    </button>
   )
 }
